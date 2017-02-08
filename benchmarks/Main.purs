c99b931a2c510adac0f310df1784ecd07607da37
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-02-08 11:53:39 +0100

    more benchmark stuff

--- benchmarks/Main.purs
+++ benchmarks/Main.purs
@@ -12,6 +12,8 @@ import Benchotron.Core
 import Benchotron.UI.Console
 
 sizeScale = (_ * 100)
+--defaultSize = (1..50) <#> sizeScale
+defaultSize = [1, 10, 20, 30, 40, 50] <#> sizeScale
 
 randomArray :: Int -> Gen (Array Number)
 randomArray = flip vectorOf arbitrary
@@ -20,7 +22,7 @@ benchFilter :: Benchmark
 benchFilter = mkBenchmark
   { slug: "filter"
   , title: "Filter the dataframe"
-  , sizes: (1..50) <#> sizeScale
+  , sizes: defaultSize
   , sizeInterpretation: "Number of rows in the dataset"
   , inputsPerSize: 1
   , gen: randomArray
@@ -35,7 +37,7 @@ benchGroup :: Benchmark
 benchGroup = mkBenchmark
   { slug: "group"
   , title: "Group the dataframe"
-  , sizes: (1..50) <#> sizeScale
+  , sizes: defaultSize
   , sizeInterpretation: "Number of rows in the dataset"
   , inputsPerSize: 1
   , gen: randomArray
@@ -50,7 +52,7 @@ benchCount :: Benchmark
 benchCount = mkBenchmark
   { slug: "count"
   , title: "Size of groups in the dataframe"
-  , sizes: (1..50) <#> sizeScale
+  , sizes: defaultSize
   , sizeInterpretation: "Number of rows in the dataset"
   , inputsPerSize: 1
   , gen: randomArray
@@ -65,7 +67,7 @@ benchSummarize :: Benchmark
 benchSummarize = mkBenchmark
   { slug: "summarize"
   , title: "Convert the dataframe to an array"
-  , sizes: (1..50) <#> sizeScale
+  , sizes: defaultSize
   , sizeInterpretation: "Number of rows in the dataset"
   , inputsPerSize: 1
   , gen: randomArray
@@ -78,7 +80,7 @@ benchTrim :: Benchmark
 benchTrim = mkBenchmark
   { slug: "trim"
   , title: "Truncate the dataframe"
-  , sizes: (1..50) <#> sizeScale
+  , sizes: defaultSize
   , sizeInterpretation: "Number of rows in the dataset"
   , inputsPerSize: 1
   , gen: randomArray
@@ -87,11 +89,29 @@ benchTrim = mkBenchmark
                ]
   }
 
+bench_groups :: Benchmark
+bench_groups = mkBenchmark
+  { slug: "_group"
+  , title: "Group the dataframe"
+  , sizes: defaultSize
+  , sizeInterpretation: "Number of rows in the dataset"
+  , inputsPerSize: 1
+  , gen: randomArray
+  , functions: [ benchFn "Array Grouping" (DF._groups f)
+               , benchFn "Seq Grouping" (DF._groups' f)
+               , benchFn "List Grouping" (DF._groups'' f)
+               -- TODO: benchmark different versions of the code if possible...
+               ]
+  }
+  where
+  f x = mod (I.floor x) 10
+
 main = runSuite
   [ benchFilter
   , benchGroup
   , benchCount
   , benchSummarize
   , benchTrim
+  , bench_groups
   ]
 
